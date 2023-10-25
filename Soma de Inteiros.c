44dfd5b97c3ed8f011a8685e30fc6d8cc7ed11f3
JOSE INACIO ARAUJO MORAES FILHO <29800347+InacioMoraes@users.noreply.github.com>
2023-10-25 08:34:15 -0300

    Add files via upload

---
+++ Soma de Inteiros.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int numb1, numb2, total;
+    printf("Digite o primeiro número:");
+    scanf("%d", &numb1);
+    printf("Digite o seugundo número:");
+    scanf("%d", &numb2);
+    total = numb1+numb2;
+    
+    printf("Total = %d", total);
+   
+    return 0;
+}
